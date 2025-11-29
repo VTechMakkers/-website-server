bb597b9637c0ec274dd8c9b90f917274d511e012
harshadkakadiya0096@gmail.com <harshadkakadiya0096@gmail.com>
2025-11-29 17:08:10 +0530

    CORS issue fixed

--- index.ts
+++ index.ts
@@ -9,14 +9,10 @@ import helmet from "helmet";
 const log = (message: string) => console.log(`[server] ${message}`);
 
 const app = express();
-//
+
 app.use(helmet());
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Referrer-Policy', 'strict-origin-when-cross-origin');
-  next();
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
